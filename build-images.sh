94eae4f0f994744449d92ff3b163892cf6aed4bd
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-05-13 16:11:49 -0400

    Fixed swarm image ref

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-docker image build -t pypi_scraper/db -f db.Dockerfile .
-docker image build -t pypi_scraper/app -f app.Dockerfile .
+docker image build \
+    -t pypi_scraper/db \
+    -t rpi-cluster-4b-1gb-1:5000/pypi_scraper/db:latest \
+    -f db.Dockerfile .
+
+docker image build \
+    -t pypi_scraper/app \
+    -t rpi-cluster-4b-1gb-1:5000/pypi_scraper/db:latest \
+    -f app.Dockerfile .
