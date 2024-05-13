bbae8988c43f1a938b5ae97b7a64bc4f2740e2c3
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-05-13 15:32:41 -0400

    Fixed build commands

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker image build -t pypi_scraper/db -f db.Dockerfile
-docker image build -t pypi_scraper/app -f app.Dockerfile
+docker image build -t pypi_scraper/db -f db.Dockerfile .
+docker image build -t pypi_scraper/app -f app.Dockerfile .
