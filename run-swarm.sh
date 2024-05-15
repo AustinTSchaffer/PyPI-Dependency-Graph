74dc1e152fba73311c96f5ae79cba3a208cea73d
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-05-15 11:14:39 -0400

    Fixed idiot mistake with swarm deploy

--- run-swarm.sh
+++ run-swarm.sh
@@ -8,14 +8,14 @@ docker image build \
     -t "rpi-cluster-4b-1gb-1:5000/pypi_scraper/db:$version" \
     -f db.Dockerfile .
 
+docker image push --all-tags rpi-cluster-4b-1gb-1:5000/pypi_scraper/db
+docker service update --image "rpi-cluster-4b-1gb-1:5000/pypi_scraper/db:$version" pypi_scraper_db
+
 docker image build \
     -t pypi_scraper/app:latest \
     -t rpi-cluster-4b-1gb-1:5000/pypi_scraper/app:latest \
     -t "rpi-cluster-4b-1gb-1:5000/pypi_scraper/app:$version" \
     -f app.Dockerfile .
 
-docker image push --all-tags rpi-cluster-4b-1gb-1:5000/pypi_scraper/db
 docker image push --all-tags rpi-cluster-4b-1gb-1:5000/pypi_scraper/app
-
-docker service update --image "rpi-cluster-4b-1gb-1:5000/pypi_scraper/app:$version" pypi_scraper_db
-docker service update --image "rpi-cluster-4b-1gb-1:5000/pypi_scraper/db:$version" pypi_scraper_app
+docker service update --image "rpi-cluster-4b-1gb-1:5000/pypi_scraper/app:$version" pypi_scraper_app
