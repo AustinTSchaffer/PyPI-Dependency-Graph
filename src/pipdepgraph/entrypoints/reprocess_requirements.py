1ea32cbc5cb5245a9376770f779cb27285334303
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-10-26 20:43:51 -0400

    More logs. Deferred commits

--- src/pipdepgraph/entrypoints/reprocess_requirements.py
+++ src/pipdepgraph/entrypoints/reprocess_requirements.py
@@ -12,7 +12,8 @@ from pipdepgraph.repositories import (
 
 DIST_ID_HASH_ALG = os.getenv("DIST_ID_HASH_ALG", "md5")
 DIST_ID_HASH_MOD_BASE = int(os.getenv("DIST_ID_HASH_MOD_BASE", "16"))
-DIST_ID_HASH_MOD_FILTER = int(os.getenv("DIST_ID_HASH_MOD_FILTER", "0")) - 1
+DIST_ID_HASH_MOD_FILTER = int(os.getenv("DIST_ID_HASH_MOD_FILTER", "1")) - 1
+COMMIT_BATCH_SIZE = int(os.getenv("COMMIT_FREQUENCY", 1000))
 
 logger = logging.getLogger("pipdepgraph.entrypoints.reprocess_requirements")
 
@@ -26,12 +27,20 @@ async def main():
         async with (db_pool.connection() as conn, conn.cursor() as edit_cursor,):
             hashmodfilter = (DIST_ID_HASH_ALG, DIST_ID_HASH_MOD_BASE, DIST_ID_HASH_MOD_FILTER)
             logger.info(f"Iterating over requirements matching hashmod filter on distribution_id: {hashmodfilter}")
+            records_updated = 0
             async for requirement in rr.iter_requirements(dist_id_hash_mod_filter=hashmodfilter):
                 requirement.dependency_extras_arr = []
                 if requirement.dependency_extras:
                     requirement.dependency_extras_arr = requirement.dependency_extras.split(',')
+                logger.info(f"Updating requirement: {requirement}")
                 await rr.update_requirement(requirement, cursor=edit_cursor)
-                await edit_cursor.execute("commit;")
+                records_updated = (records_updated + 1) % COMMIT_BATCH_SIZE
+                if records_updated == 0:
+                    logger.info(f"Committing batch of size: {COMMIT_BATCH_SIZE}")
+                    await edit_cursor.execute("commit;")
+                    logger.info(f"Commit completed.")
+
+            await edit_cursor.execute("commit;")
 
 
 if __name__ == "__main__":
