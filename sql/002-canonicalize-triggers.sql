5b88f87c59334e35700d97fd13cb96a4bc74ab44
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-11-12 15:45:00 -0500

    Added CDC function/triggers

--- sql/002-canonicalize-triggers.sql
+++ sql/002-canonicalize-triggers.sql
@@ -1,46 +1,46 @@
 create or replace function pypi_packages.canonicalize_package_name(package_name text)
-  	returns text as
-	$body$
-	begin
-		return lower(regexp_replace(package_name, '[-_\.]+', '-', 'g'));
-	end;
-	$body$
+    returns text as
+    $body$
+    begin
+        return lower(regexp_replace(package_name, '[-_\.]+', '-', 'g'));
+    end;
+    $body$
 language plpgsql;
 
 create or replace function pypi_packages.canonicalize_package_name_tr()
-  	returns trigger as
-	$body$
-	begin
-	   new.package_name = pypi_packages.canonicalize_package_name(new.package_name);
-	   return new;
-	end;
-	$body$
+    returns trigger as
+    $body$
+    begin
+       new.package_name = pypi_packages.canonicalize_package_name(new.package_name);
+       return new;
+    end;
+    $body$
 language plpgsql;
 
 create or replace function pypi_packages.canonicalize_dependency_name_tr()
-  	returns trigger as
-	$body$
-	begin
-	   new.dependency_name = pypi_packages.canonicalize_package_name(new.dependency_name);
-	   return new;
-	end;
-	$body$
+    returns trigger as
+    $body$
+    begin
+       new.dependency_name = pypi_packages.canonicalize_package_name(new.dependency_name);
+       return new;
+    end;
+    $body$
 language plpgsql;
 
 create or replace trigger canonicalize_package_name
-	before insert or update
-	on pypi_packages.package_names
-	for each row
-	execute function pypi_packages.canonicalize_package_name_tr();
+    before insert or update
+    on pypi_packages.package_names
+    for each row
+    execute function pypi_packages.canonicalize_package_name_tr();
 
 create or replace trigger canonicalize_package_name
-	before insert or update
-	on pypi_packages.versions
-	for each row
-	execute function pypi_packages.canonicalize_package_name_tr();
+    before insert or update
+    on pypi_packages.versions
+    for each row
+    execute function pypi_packages.canonicalize_package_name_tr();
 
 create or replace trigger canonicalize_dependency_name
-	before insert or update
-	on pypi_packages.requirements
-	for each row
-	execute function pypi_packages.canonicalize_dependency_name_tr();
+    before insert or update
+    on pypi_packages.requirements
+    for each row
+    execute function pypi_packages.canonicalize_dependency_name_tr();
--- sql/007-cdc.sql
+++ sql/007-cdc.sql
@@ -9,8 +9,8 @@ create table if not exists cdc.event_log (
     "operation" text not null,
     "schema" text not null,
     "table" text not null,
-    "before" jsonb,
-    "after" jsonb,
+    "before" json,
+    "after" json,
     "timestamp" timestamp not null default now()
 );
 
@@ -19,3 +19,58 @@ create table if not exists cdc.offsets (
     "table" text not null,
     "event_id" bigint not null
 );
+
+create or replace function cdc.event_log_insert_tr()
+    returns trigger as $body$
+    begin
+        insert into cdc.event_log (
+            "operation",
+            "schema",
+            "table",
+            "before",
+            "after"
+        ) values (
+            tg_op::text,
+            tg_table_schema::text,
+            tg_table_name::text,
+            row_to_json(old),
+            row_to_json(new)
+        );
+
+        if tg_op = 'DELETE' then
+            return old;
+        else
+            return new;
+        end if;
+    end;
+$body$ language plpgsql;
+
+create or replace trigger cdc_event_log_insert
+    before insert or update or delete
+    on pypi_packages.package_names
+    for each row
+    execute function cdc.event_log_insert_tr();
+
+create or replace trigger cdc_event_log_insert
+    before insert or update or delete
+    on pypi_packages.versions
+    for each row
+    execute function cdc.event_log_insert_tr();
+
+create or replace trigger cdc_event_log_insert
+    before insert or update or delete
+    on pypi_packages.distributions
+    for each row
+    execute function cdc.event_log_insert_tr();
+
+create or replace trigger cdc_event_log_insert
+    before insert or update or delete
+    on pypi_packages.requirements
+    for each row
+    execute function cdc.event_log_insert_tr();
+
+create or replace trigger cdc_event_log_insert
+    before insert or update or delete
+    on pypi_packages.candidates
+    for each row
+    execute function cdc.event_log_insert_tr();
