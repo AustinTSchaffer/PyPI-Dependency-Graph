df2942fb9a3395691a085ecd39bf729c6f25ec10
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-11-12 15:26:35 +0000

    Started frame-out of custom cdc

--- sql/007-cdc.sql
+++ sql/007-cdc.sql
@@ -1,5 +1,21 @@
 create schema cdc;
 grant all on schema cdc to pypi_scraper;
 
-create user debezium with password 'password';
-grant select on all tables in schema cdc to debezium;
+create user cdc_user with password 'password';
+grant select update on all tables in schema cdc to cdc_user;
+
+create table if not exists cdc.event_log (
+    "event_id" bigserial primary key,
+    "operation" text not null,
+    "schema" text not null,
+    "table" text not null,
+    "before" jsonb,
+    "after" jsonb,
+    "timestamp" timestamp not null default now()
+);
+
+create table if not exists cdc.offsets (
+    "schema" text not null,
+    "table" text not null,
+    "event_id" bigint not null
+);
